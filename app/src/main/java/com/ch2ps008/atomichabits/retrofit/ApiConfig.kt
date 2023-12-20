decfd831a73398ed611c221501a3427976ff5f90
Andrico <103520236+AndricoHalim@users.noreply.github.com>
2023-12-20 23:04:54 +0700

    remove unused resource

--- app/src/main/java/com/ch2ps008/atomichabits/retrofit/ApiConfig.kt
+++ app/src/main/java/com/ch2ps008/atomichabits/retrofit/ApiConfig.kt
@@ -10,7 +10,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 class ApiConfig {
     companion object {
         private const val BASE_URL = BuildConfig.BASE_URL
-        private const val BASE_URL2 = BuildConfig.BASE_URL2
         fun getApiService(): ApiService {
             val loggingInterceptor =
                 if (BuildConfig.DEBUG) {
@@ -28,25 +27,5 @@ class ApiConfig {
                 .build()
             return retrofit.create(ApiService::class.java)
         }
-    }
-
-    fun getPredictApi(): ApiService {
-        val loggingInterceptor = if (BuildConfig.DEBUG) {
-            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-        } else {
-            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
-        }
-
-        val client = OkHttpClient.Builder()
-            .addInterceptor(loggingInterceptor)
-            .build()
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL2)
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(client)
-            .build()
-
-        return retrofit.create(ApiService::class.java)
     }
 }
