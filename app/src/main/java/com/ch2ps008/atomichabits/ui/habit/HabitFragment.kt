f22bded7f49039cecfb4da2edf7a3af53585d658
Andrico <103520236+AndricoHalim@users.noreply.github.com>
2023-12-20 14:14:50 +0700

    update

--- app/src/main/java/com/ch2ps008/atomichabits/ui/habit/HabitFragment.kt
+++ app/src/main/java/com/ch2ps008/atomichabits/ui/habit/HabitFragment.kt
@@ -60,7 +60,7 @@ class HabitFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentHabitBinding.inflate(inflater, container, false)
         return binding.root
     }
